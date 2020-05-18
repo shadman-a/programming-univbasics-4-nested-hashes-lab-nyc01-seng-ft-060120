ae55f6ef30c1f2623820c925014fd178cbb5211f
Shadman Ahmed <shadman@outlook.com>
2020-05-18 02:35:20 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -141,7 +141,7 @@ def adding_to_dennis
       }
     }
 
-programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+programmer_hash[:dennis_ritchie][:languages]<<["Assembly"]
 
 programmer_hash
 
